c626755c048b656d73644aa22fa9a80f5f5c9d15
Garrett Lietzen <glietzen@gmail.com>
2018-10-10 11:52:18 -0500

    init

---
+++ announcementCDN.js
@@ -0,0 +1,34 @@
+function getAnnouncements() {
+    $('.card-test').empty();
+    $.ajax({
+        url: `http://mymillcreek.herokuapp.com/api/announcements`,
+        method: `GET`,
+        dataType: 'jsonp'
+    }).then((res) => {
+        for (var i = 0; i < res.length; i++) {
+            let card = '';
+
+            card+= `
+            <div class="container py-3">
+            <div class="card">
+                <div class="row ">
+                    <div class="col-lg-4">
+                        
+                    </div>
+                    <div class="col-lg-8 px-3">
+                        <div class="card-block px-3">
+                            <h5 class="card-title mc-title pt-3">${res[i].title}</h5>
+                            <p class="card-text">${res[i].description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+
+
+            $('.card-test').prepend(card)
+        }
+    })
+}
+
+getAnnouncements();
