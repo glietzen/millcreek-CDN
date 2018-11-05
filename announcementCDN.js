b50bf297ae7dc7de79fc572ca44a5a4e47d346b9
Garrett Lietzen <glietzen@gmail.com>
2018-11-05 12:33:27 -0600

    Update announcementCDN.js

--- announcementCDN.js
+++ announcementCDN.js
@@ -16,23 +16,12 @@ function getAnnouncements() {
             let card = '';
             card += `
             <div class="container py-3">
-            <div class="card">
-                <div class="row ">
-                    <div class="col-lg-4">
-                        
-                    </div>
-                    <div class="col-lg-8 px-3">
-                        <div class="card-block px-3">
-                            <h5 class="card-title mc-title pt-3">${res[i].title}</h5>
-                            <p class="card-text">${res[i].description}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`
+                <h5 class="card-title mc-title pt-3">${res[i].title}</h5>
+                <p class="card-text">${res[i].description}</p>
+            </div>`
 
 
-            $('.card-test').prepend(card)
+            $('.card-test').append(card)
         }
     })
 }
